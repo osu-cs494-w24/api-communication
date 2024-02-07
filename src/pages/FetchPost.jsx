ed1199e3e9f828b060977a23bb2345bc62782a80
Rob Hess <hessro@oregonstate.edu>
2024-02-07 15:24:19 -0800

    Add code from 2/7 lecture.

--- src/pages/FetchPost.jsx
+++ src/pages/FetchPost.jsx
@@ -5,9 +5,30 @@ const userId = 1234
 export default function Post() {
     const [ title, setTitle ] = useState("")
     const [ body, setBody ] = useState("")
+
+    async function sendPost() {
+        if (body && title && userId) {
+            const res = await fetch(
+                "https://jsonplaceholder.typicode.com/posts",
+                {
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        title: title,
+                        body: body,
+                        userId: userId
+                    })
+                }
+            )
+            const resBody = await res.json()
+            console.log("== resBody:", resBody)
+        }
+    }
+
     return (
         <form onSubmit={e => {
             e.preventDefault()
+            sendPost()
         }}>
             <div>
                 <input placeholder="Title" value={title} onChange={e => setTitle(e.target.value)} />
--- src/pages/FetchSearch.jsx
+++ src/pages/FetchSearch.jsx
@@ -1,10 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { useSearchParams } from 'react-router-dom'
+
+import Spinner from '../components/Spinner'
+import ErrorContainer from '../components/ErrorContainer'
 
 export default function Search() {
     const [ searchParams, setSearchParams ] = useSearchParams()
     const query = searchParams.get("q")
     const [ inputQuery, setInputQuery ] = useState(query || "")
+    const [ repos, setRepos ] = useState([])
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(null)
+
+    useEffect(() => {
+        const controller = new AbortController()
+        async function fetchSearchResults() {
+            setLoading(true)
+            try {
+                // throw new Error("woops!")
+                const response = await fetch(
+                    `https://api.github.com/search/repositories?q=${query}`,
+                    { signal: controller.signal }
+                )
+                const responseBody = await response.json()
+                console.log("== responseBody:", responseBody)
+                setLoading(false)
+                setError(null)
+                setRepos(responseBody.items || [])
+            } catch (err) {
+                if (err.name === "AbortError") {
+                    console.log("HTTP request was aborted")
+                } else {
+                    console.error(err)
+                    setError(err)
+                }
+            }
+        }
+        if (query) {
+            fetchSearchResults()
+        }
+        return () => controller.abort()
+    }, [ query ])
 
     return (
         <div>
@@ -16,6 +52,15 @@ export default function Search() {
                 <button type="submit">Search</button>
             </form>
             <h2>Search query: {query}</h2>
+            {error && <ErrorContainer>Error: {error.message}</ErrorContainer>}
+            {loading && <Spinner />}
+            <ul>
+                {repos.map(repo => (
+                    <li key={repo.id}>
+                        <a href={repo.html_url}>{repo.full_name}</a>
+                    </li>
+                ))}
+            </ul>
         </div>
     )
 }
